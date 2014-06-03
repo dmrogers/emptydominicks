9a00d3ce978321d07bcdbec42be40d40d47c08f4
dmrogers <dmrogers9@comcast.net>
2014-06-03 16:04:28 -0500

    Added a navbar!
    Working on making it work for all browser sizes.

--- website/script.js
+++ website/script.js
@@ -9,6 +9,52 @@ $( "a" ).hover(
         $( this ).removeClass("hover")
     });   
 
+$( "li" ).hover(
+    function () {
+        $( this ).addClass("hover")
+    },
+    function () {
+        $( this ).removeClass("hover")
+    },
+    function () {
+      $( "body" ).css('cursor', 'pointer')
+    });   
+
+$("#a").click(
+    function() {
+        $('html, body').animate({
+           scrollTop: $("#subtitle").offset().top
+    }, 2000);
+});
+
+$("#b").click(
+    function() {
+        $('html, body').animate({
+           scrollTop: $("#poverty").offset().top
+    }, 2000);
+});
+
+$("#c").click(
+    function() {
+        $('html, body').animate({
+           scrollTop: $("#anchor").offset().top
+    }, 2000);
+});
+
+$("#d").click(
+    function() {
+        $('html, body').animate({
+           scrollTop: $("#health").offset().top
+    }, 2000);
+});
+
+$("#e").click(
+    function() {
+        $('html, body').animate({
+           scrollTop: $("#sanity").offset().top
+    }, 2000);
+});
+
 $(function() {
     $("img.lazy").lazyload({
         threshold : 500
