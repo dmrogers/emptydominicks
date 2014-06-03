ea1dc2823177a291dd8e45f795e791030b47872b
dmrogers <dmrogers9@comcast.net>
2014-06-03 15:06:30 -0500

    Got 'em. Should be as it was.
    Had to delete a lot of changes, but learned some stuff in the process.
    Gonna fix it more, but this should all be working as intended, or at
    least close to it.

--- website/script.js
+++ website/script.js
@@ -1,84 +1,6 @@
 $(document).ready(function(){
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1c9627d555cf0f632d87207aba2983b026c40aaf
-  var win = $(window),
-      fullscreen = $('#headerimage'),
-      image = fullscreen.find('fadingsign.jpg"'),
-      imageWidth = image.width(),
-      imageHeight = image.height(),
-      imageRatio = imageWidth / imageHeight;
 
-  win.bind({
-    load: function() {
-      resizeImage();
-    },
-    resize: function() {
-      resizeImage();
-    }
-  });
-<<<<<<< HEAD
-=======
-var $img = $('#headerimage'),
-    imageWidth = $img[0].width, //need the raw width due to a jquery bug that affects chrome
-    imageHeight = $img[0].height, //need the raw height due to a jquery bug that affects chrome
-    maxWidth = $(window).width(),
-    maxHeight = $(window).height(),
-    widthRatio = maxWidth / imageWidth,
-    heightRatio = maxHeight / imageHeight;
->>>>>>> parent of 61181cd... Made the title card
-
-var ratio = widthRatio; //default to the width ratio until proven wrong
-
-if (widthRatio * imageHeight > maxHeight) {
-    ratio = heightRatio;
-}
-
-console.write('functional');
-
-//now resize the image relative to the ratio
-$img.attr('width', imageWidth * ratio)
-    .attr('height', imageHeight * ratio);
-
-//and center the image vertically and horizontally
-$img.css({
-    margin: 'auto',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
-=======
-
->>>>>>> 1c9627d555cf0f632d87207aba2983b026c40aaf
-});
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1c9627d555cf0f632d87207aba2983b026c40aaf
-  function resizeImage() {
-    var winWidth = win.width(),
-        winHeight = win.height(),
-        winRatio = winWidth / winHeight;
-  
-    if(winRatio > imageRatio) {
-      image.css({
-        width: winWidth,
-        height: Math.round(winWidth / imageRatio)
-      });
-    } else {
-      image.css({
-        width: Math.round(winHeight * imageRatio),
-        height: winHeight
-      });
-    }
-  }
-=======
 $( "a" ).hover(
     function () {
         $( this ).addClass("hover")
@@ -95,9 +17,3 @@ $(function() {
 
 
 });
->>>>>>> FETCH_HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 61181cd... Made the title card
-=======
->>>>>>> 1c9627d555cf0f632d87207aba2983b026c40aaf
