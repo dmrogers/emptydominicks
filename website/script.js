61181cd265e6e347dc53baf261e1312cedb35849
Pantsworth <nski6m@gmail.com>
2014-06-03 02:16:47 -0500

    Made the title card
    Made the title card. Modified the header div and some CSS. No
    Javascript needed :)

--- website/script.js
+++ website/script.js
@@ -1,34 +1,38 @@
 $(document).ready(function(){
 
-var $img = $('#headerimage'),
-    imageWidth = $img[0].width, //need the raw width due to a jquery bug that affects chrome
-    imageHeight = $img[0].height, //need the raw height due to a jquery bug that affects chrome
-    maxWidth = $(window).width(),
-    maxHeight = $(window).height(),
-    widthRatio = maxWidth / imageWidth,
-    heightRatio = maxHeight / imageHeight;
+  var win = $(window),
+      fullscreen = $('#headerimage'),
+      image = fullscreen.find('fadingsign.jpg"'),
+      imageWidth = image.width(),
+      imageHeight = image.height(),
+      imageRatio = imageWidth / imageHeight;
 
-var ratio = widthRatio; //default to the width ratio until proven wrong
+  win.bind({
+    load: function() {
+      resizeImage();
+    },
+    resize: function() {
+      resizeImage();
+    }
+  });
 
-if (widthRatio * imageHeight > maxHeight) {
-    ratio = heightRatio;
-}
-
-console.write('functional');
-
-//now resize the image relative to the ratio
-$img.attr('width', imageWidth * ratio)
-    .attr('height', imageHeight * ratio);
-
-//and center the image vertically and horizontally
-$img.css({
-    margin: 'auto',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0
 });
 
-	});
 
+  function resizeImage() {
+    var winWidth = win.width(),
+        winHeight = win.height(),
+        winRatio = winWidth / winHeight;
+  
+    if(winRatio > imageRatio) {
+      image.css({
+        width: winWidth,
+        height: Math.round(winWidth / imageRatio)
+      });
+    } else {
+      image.css({
+        width: Math.round(winHeight * imageRatio),
+        height: winHeight
+      });
+    }
+  }
