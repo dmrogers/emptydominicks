973ad7c9a1f9f225bec70f44acad0a8a3a91aeb6
dmrogers <dmrogers9@comcast.net>
2014-06-03 12:52:22 -0500

    Reverted to working state.

--- website/script.js
+++ website/script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
 
+<<<<<<< HEAD
+<<<<<<< HEAD
+  var win = $(window),
+      fullscreen = $('#headerimage'),
+      image = fullscreen.find('fadingsign.jpg"'),
+      imageWidth = image.width(),
+      imageHeight = image.height(),
+      imageRatio = imageWidth / imageHeight;
+
+  win.bind({
+    load: function() {
+      resizeImage();
+    },
+    resize: function() {
+      resizeImage();
+    }
+  });
+=======
 var $img = $('#headerimage'),
     imageWidth = $img[0].width, //need the raw width due to a jquery bug that affects chrome
     imageHeight = $img[0].height, //need the raw height due to a jquery bug that affects chrome
@@ -7,6 +25,7 @@ var $img = $('#headerimage'),
     maxHeight = $(window).height(),
     widthRatio = maxWidth / imageWidth,
     heightRatio = maxHeight / imageHeight;
+>>>>>>> parent of 61181cd... Made the title card
 
 var ratio = widthRatio; //default to the width ratio until proven wrong
 
@@ -32,3 +51,41 @@ $img.css({
 
 	});
 
+<<<<<<< HEAD
+  function resizeImage() {
+    var winWidth = win.width(),
+        winHeight = win.height(),
+        winRatio = winWidth / winHeight;
+  
+    if(winRatio > imageRatio) {
+      image.css({
+        width: winWidth,
+        height: Math.round(winWidth / imageRatio)
+      });
+    } else {
+      image.css({
+        width: Math.round(winHeight * imageRatio),
+        height: winHeight
+      });
+    }
+  }
+=======
+$( "a" ).hover(
+    function () {
+        $( this ).addClass("hover")
+    },
+    function () {
+        $( this ).removeClass("hover")
+    });   
+
+$(function() {
+    $("img.lazy").lazyload({
+        threshold : 500
+    });
+});
+
+
+});
+>>>>>>> FETCH_HEAD
+=======
+>>>>>>> parent of 61181cd... Made the title card
